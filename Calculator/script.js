f582af62a54ac68dfd167db088179da4048152e8
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-09-28 12:41:57 +0530

    calculator project completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,23 @@
+// Declarations
 
-function insert(num){
-  }
-  
-  function equals(){
-    
-      
-  }
-  
-  function clean(){
-  }
-  
-  function back(){
-  }
+let screen = document.querySelector(".textview");
+let string = "";
+function insert(num) {
+  string = string + num;
+  screen.value = string;
+}
+
+function equals() {
+  string = eval(string);
+  screen.value = string;
+}
+
+function clean() {
+  string = "";
+  screen.value = string;
+}
+
+function back() {
+  string = string.slice(0, -1);
+  screen.value = string;
+}
